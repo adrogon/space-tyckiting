a7aba7fb6387688b657848a4985289888ff39889
Anthony Drogon <anthony.drogon@gmail.com>
2016-04-13 18:00:02 +0200

    Improve everything

--- clients/python/tyckiting_client/ai/adrogon.py
+++ clients/python/tyckiting_client/ai/adrogon.py
@@ -29,38 +29,42 @@ class Ai(base.BaseAi):
         #    for event in events:
         #        logging.info(event)
 
+        boots = []
+        for bot in bots:
+            if bot.alive:
+                boots.append(bot)
+
         response = []
 
         radar_positions = [
             [0, -11],
-            [4, -11],
-            [8, -11],
-            [11, -11],
             [11, -8],
-            [-4, -7],
             [0, -4],
-            [4, -4],
+            [4, 3],
             [8, -4],
             [11, -4],
+            [0, 7],
             [-11, 0],
+            [-4, 7],
             [-11, 3],
+            [4, -11],
             [11, 0],
             [-8, -3],
+            [4, -4],
+            [8, -11],
             [-4, 0],
+            [11, -11],
             [0, 3],
-            [4, 3],
+            [-4, -7],
             [8, 3],
             [-11, 7],
             [-8, 7],
-            [-4, 7],
-            [0, 7],
             [4, 7]
         ]
 
-
         for event in events:
             if event.event in ['detected', 'damaged']:
-                for bot in bots:
+                for bot in boots:
                     if bot.bot_id != event.bot_id:
                         continue
                     move_pos = random.choice(list(self.get_valid_moves(bot)))
@@ -69,32 +73,49 @@ class Ai(base.BaseAi):
                                                  y=move_pos.y))
 
             if event.event in ['radarEcho']:
-                if len(bots) == 3:
-                    response.append(actions.Radar(bot_id=bots[0].bot_id, x=event.pos.x, y=event.pos.y))
+                if len(boots) > 2:
+                    response.append(actions.Radar(bot_id=boots[0].bot_id, x=event.pos.x, y=event.pos.y))
                     rand_int = random.randint(0, 2)
                     if rand_int == 0:
-                        response.append(actions.Cannon(bot_id=bots[1].bot_id, x=event.pos.x, y=event.pos.y + 1))
-                        response.append(actions.Cannon(bot_id=bots[2].bot_id, x=event.pos.x, y=event.pos.y - 1))
+                        response.append(actions.Cannon(bot_id=boots[1].bot_id, x=event.pos.x, y=event.pos.y + 1))
+                        response.append(actions.Cannon(bot_id=boots[2].bot_id, x=event.pos.x, y=event.pos.y - 1))
                     elif rand_int == 1:
-                        response.append(actions.Cannon(bot_id=bots[1].bot_id, x=event.pos.x + 1, y=event.pos.y - 1))
-                        response.append(actions.Cannon(bot_id=bots[2].bot_id, x=event.pos.x - 1, y=event.pos.y + 1))
+                        response.append(actions.Cannon(bot_id=boots[1].bot_id, x=event.pos.x + 1, y=event.pos.y - 1))
+                        response.append(actions.Cannon(bot_id=boots[2].bot_id, x=event.pos.x - 1, y=event.pos.y + 1))
                     elif rand_int == 2:
-                        response.append(actions.Cannon(bot_id=bots[1].bot_id, x=event.pos.x - 1, y=event.pos.y))
-                        response.append(actions.Cannon(bot_id=bots[2].bot_id, x=event.pos.x + 1, y=event.pos.y))
+                        response.append(actions.Cannon(bot_id=boots[1].bot_id, x=event.pos.x - 1, y=event.pos.y))
+                        response.append(actions.Cannon(bot_id=boots[2].bot_id, x=event.pos.x + 1, y=event.pos.y))
 
-                    logging.info("bots shooting around %s,%s", event.pos.x, event.pos.y)
+                elif len(boots) > 1:
+                    response.append(actions.Radar(bot_id=boots[0].bot_id, x=event.pos.x, y=event.pos.y))
+                    rand_int = random.randint(0, 2)
+                    if rand_int == 0:
+                        response.append(actions.Cannon(bot_id=boots[1].bot_id, x=event.pos.x, y=event.pos.y + 1))
+                    elif rand_int == 1:
+                        response.append(actions.Cannon(bot_id=boots[1].bot_id, x=event.pos.x + 1, y=event.pos.y - 1))
+                    elif rand_int == 2:
+                        response.append(actions.Cannon(bot_id=boots[1].bot_id, x=event.pos.x - 1, y=event.pos.y))
 
+                else:
+                    rand_int = random.randint(0, 2)
+                    if rand_int == 0:
+                        response.append(actions.Cannon(bot_id=boots[0].bot_id, x=event.pos.x, y=event.pos.y + 1))
+                    elif rand_int == 1:
+                        response.append(actions.Cannon(bot_id=boots[0].bot_id, x=event.pos.x + 1, y=event.pos.y - 1))
+                    elif rand_int == 2:
+                        response.append(actions.Cannon(bot_id=boots[0].bot_id, x=event.pos.x - 1, y=event.pos.y))
 
         # Default action
-        radar_pos = random.choice(list(self.get_valid_radars(bots[0])))
+        another_rand_int = random.randint(0, len(radar_positions) - 2)
 
-        bot_one = [radar_pos.x+7, radar_pos.y-3]
-        bot_two = [radar_pos.x +3, radar_pos.y + 4]
-
-        response.append(actions.Radar(bot_id=bots[0].bot_id, x=radar_pos.x , y=radar_pos.y))
-        response.append(actions.Radar(bot_id=bots[1].bot_id, x=bot_one[0], y=bot_one[1]))
-        response.append(actions.Radar(bot_id=bots[2].bot_id, x=bot_two[0], y=bot_two[1]))
-
-
+        response.append(
+                actions.Radar(bot_id=boots[0].bot_id, x=radar_positions[another_rand_int][0],
+                              y=radar_positions[another_rand_int][1]))
+        if len(boots) > 1:
+            response.append(actions.Radar(bot_id=boots[1].bot_id, x=radar_positions[another_rand_int + 1][0],
+                                          y=radar_positions[another_rand_int + 1][1]))
+        if len(boots) > 2:
+            response.append(actions.Radar(bot_id=boots[2].bot_id, x=radar_positions[another_rand_int + 2][0],
+                                          y=radar_positions[another_rand_int + 2][1]))
 
         return response
